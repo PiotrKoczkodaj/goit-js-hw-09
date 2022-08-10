26c798c57f9eaed3694514d60b47a3b6fb281479
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-08-10 23:00:57 +0200

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const changeBackgroudColor = () => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+};
+
+const stopChanging = () => { 
+    clearInterval(timerId);  
+};
+
+btnStart.addEventListener("click",()=> timerId = setInterval(changeBackgroudColor, 1000));
+btnStop.addEventListener("click",stopChanging);
