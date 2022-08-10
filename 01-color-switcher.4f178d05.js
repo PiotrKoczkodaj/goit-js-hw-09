9d03b1ea3952291a568b548b20e1a238ea78ca86
PiotrKoczkodaj <101861936+PiotrKoczkodaj@users.noreply.github.com>
2022-08-10 21:01:28 +0000

    Deploying to gh-pages from @ PiotrKoczkodaj/goit-js-hw-09@26c798c57f9eaed3694514d60b47a3b6fb281479 🚀

---
+++ 01-color-switcher.4f178d05.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;var o=function(){document.body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))};t.addEventListener("click",(function(){return e=setInterval(o,1e3)})),n.addEventListener("click",(function(){clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.4f178d05.js.map
